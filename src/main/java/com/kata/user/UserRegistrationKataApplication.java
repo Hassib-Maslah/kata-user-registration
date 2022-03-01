6de450dbe27e64803b8e29f64506c61f47a99684
Hassib Maslah <maslah.has@gmail.com>
2022-03-01 17:43:15 +0100

    [ADD] initialize the project

---
+++ src/main/java/com/kata/user/UserRegistrationKataApplication.java
@@ -0,0 +1,13 @@
+package com.kata.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserRegistrationKataApplication.class, args);
+    }
+
+}
