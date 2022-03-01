d89e4f1a0b6dbf8b92de4c437dd7f8badae488ef
Hassib Maslah <maslah.has@gmail.com>
2022-03-01 20:36:15 +0100

    [ADD] add user registration api integration test

---
+++ src/test/java/com/kata/user/UserRegistrationIntegrationTest.java
@@ -0,0 +1,34 @@
+package com.kata.user;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class UserRegistrationIntegrationTest {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    void makeUserRegistration_returnsRegisteredUser() {
+        // arrange
+        UserRegistrationDTO request = new UserRegistrationDTO();
+        // act
+        ResponseEntity<UserRegistrationDTO> response = testRestTemplate.postForEntity("/user-management/users", request, UserRegistrationDTO.class);
+        // assert
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(response.getBody().getId()).isNotNull();
+        assertThat(response.getBody().getUsername()).isEqualTo(request.getUsername());
+    }
+
+}
