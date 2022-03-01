b0fbbae854c3324e825ec0e9914ef491e54d45d4
Hassib Maslah <maslah.has@gmail.com>
2022-03-01 22:26:56 +0100

    [UPDATE] update user controller class

--- src/main/java/com/kata/user/controller/UserController.java
+++ src/main/java/com/kata/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.kata.user.controller;
 
 import com.kata.user.model.UserDTO;
+import com.kata.user.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
