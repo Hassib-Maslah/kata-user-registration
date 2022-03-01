144e5ecee8400304bd350b92a00e4b6ef41a3d98
Hassib Maslah <maslah.has@gmail.com>
2022-03-02 00:12:13 +0100

    [UPDATE] update user controller's url

--- src/main/java/com/kata/user/controller/UserController.java
+++ src/main/java/com/kata/user/controller/UserController.java
@@ -6,14 +6,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.kata.user.constants.ApiUrlConstant.USER_PREFIX_API;
 import static com.kata.user.constants.ApiUrlConstant.USER_REGISTRATION_API;
 
 @RestController
-@RequestMapping(USER_PREFIX_API)
 public class UserController {
 
     private final UserService userService;
