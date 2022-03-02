b2c9124fa80b3aa60278a5cda88dbd0d5adece19
Hassib Maslah <maslah.has@gmail.com>
2022-03-02 18:02:19 +0100

    [UPDATE] add get user details repository unit test

--- src/test/java/com/kata/user/dao/repository/UserRepositoryTest.java
+++ src/test/java/com/kata/user/dao/repository/UserRepositoryTest.java
@@ -1,7 +1,10 @@
 package com.kata.user.dao.repository;
 
+import com.kata.user.bootstrap.UserBootstrap;
 import com.kata.user.constants.GenderEnum;
 import com.kata.user.dao.entity.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +13,11 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
@@ -21,6 +26,25 @@ public class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    private User user;
+
+    @BeforeEach
+    public void setUp() {
+        String username = "Mikel";
+        LocalDate birthday = LocalDate.of(1984, 1, 23);
+        String country = "France";
+        String phone = "0033582446557";
+        GenderEnum gender = GenderEnum.MALE;
+        user = new User(username, birthday, country, phone, gender);
+        userRepository.save(user);
+    }
+
+    @AfterEach
+    public void clear() {
+        user = null;
+        userRepository.deleteAll();
+    }
 
     @Test
     public void givenUserToAddShouldReturnAddedUser() {
@@ -41,6 +65,21 @@ public class UserRepositoryTest {
         assertThat(savedUser.getCountry()).isEqualTo(country);
         assertThat(savedUser.getPhone()).isEqualTo(phone);
         assertThat(savedUser.getGender()).isEqualTo(gender);
+    }
+
+    @Test
+    public void givenUserIdShouldReturnUserDetails() {
+        // arrange
+        Long userId = 1L;
+        // act
+        Optional<User> userOptional = userRepository.findById(userId);
+        // assert
+        assertTrue(userOptional.isPresent());
+        assertThat(userOptional.get().getUsername()).isEqualTo(user.getUsername());
+        assertThat(userOptional.get().getBirthday()).isEqualTo(user.getBirthday());
+        assertThat(userOptional.get().getCountry()).isEqualTo(user.getCountry());
+        assertThat(userOptional.get().getPhone()).isEqualTo(user.getPhone());
+        assertThat(userOptional.get().getGender()).isEqualTo(user.getGender());
     }
 
 }
