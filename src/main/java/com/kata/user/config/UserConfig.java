7d8861b01b4fc9cdc9159dc07d65c8f9c09ef2f2
Hassib Maslah <maslah.has@gmail.com>
2022-03-10 22:35:15 +0100

    [UPDATE] add javadoc to config class

--- src/main/java/com/kata/user/config/UserConfig.java
+++ src/main/java/com/kata/user/config/UserConfig.java
@@ -4,6 +4,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * This is a configuration class that contains Spring {@link Bean} definition methods.
+ */
 @Configuration
 public class UserConfig {
 
