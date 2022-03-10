2d3d78707f0076b4ba4fc1d6e474842c468fca5b
Hassib Maslah <maslah.has@gmail.com>
2022-03-10 17:57:06 +0100

    [FIX] remove unused methods

--- src/main/java/com/kata/user/utils/validation/BirthdayValidator.java
+++ src/main/java/com/kata/user/utils/validation/BirthdayValidator.java
@@ -7,10 +7,6 @@ import java.time.LocalDate;
 public class BirthdayValidator implements ConstraintValidator<BirthdayConstraint, LocalDate> {
 
     @Override
-    public void initialize(BirthdayConstraint contactNumber) {
-    }
-
-    @Override
     public boolean isValid(LocalDate birthdate, ConstraintValidatorContext cxt) {
 
         return birthdate != null && birthdate.isBefore(LocalDate.now().minusYears(18));
--- src/main/java/com/kata/user/utils/validation/CountryValidator.java
+++ src/main/java/com/kata/user/utils/validation/CountryValidator.java
@@ -7,10 +7,6 @@ import java.util.Locale;
 public class CountryValidator implements ConstraintValidator<CountryConstraint, String> {
 
     @Override
-    public void initialize(CountryConstraint contactNumber) {
-    }
-
-    @Override
     public boolean isValid(String country, ConstraintValidatorContext cxt) {
 
         return country != null && country.equalsIgnoreCase(Locale.FRANCE.getDisplayCountry());
