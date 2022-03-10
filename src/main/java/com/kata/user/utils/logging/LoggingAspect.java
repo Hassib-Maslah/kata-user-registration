d7d5ccdec1b133814eff131b15a626169a4cf815
Hassib Maslah <maslah.has@gmail.com>
2022-03-10 17:56:46 +0100

    [FIX] fix logging so that work correctly in multi-threading environment

--- src/main/java/com/kata/user/utils/logging/LoggingAspect.java
+++ src/main/java/com/kata/user/utils/logging/LoggingAspect.java
@@ -3,6 +3,7 @@ package com.kata.user.utils.logging;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.kata.user.utils.JsonUtils;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,60 +19,77 @@ import org.springframework.util.ObjectUtils;
 public class LoggingAspect {
     Logger log = LoggerFactory.getLogger(LoggingAspect.class);
 
-    private long start;
-
     /**
-     * This method executed before execution of each method in the classes under web/controller package.
-     * It initializes start time of the execution of the method.
+     * This method executed around the execution of each method in the classes under web/controller package.
+     * It contains logging of the method signature and its execution time.
      */
-    @Before("execution(* com.kata.user.web.controller.*.*(..))")
-    public void beforeExecution() {
+    @Around("execution(* com.kata.user.web.controller.*.*(..))")
+    public Object beforeExecution(ProceedingJoinPoint joinPoint) throws Throwable {
         log.info("==============Start Logging==============");
-        start = System.currentTimeMillis();
+        long start = System.currentTimeMillis();
+
+        Object proceed = joinPoint.proceed();
+
+        long executionTime = System.currentTimeMillis() - start;
+
+        log.info("Method: {} executed in {} ms", joinPoint.getSignature(), executionTime);
+
+        return proceed;
     }
 
     /**
      * This method executed after a successful execution of each method in the classes under web/controller package.
      * It contains logging of the input and output of the method.
-     * @param joinPoint {@link JoinPoint}
-     * @param returnValue  {@link Object}
+     *
+     * @param joinPoint   {@link JoinPoint}
+     * @param returnValue {@link Object}
      * @throws JsonProcessingException {@link JsonProcessingException}
      */
     @AfterReturning(value = "execution(* com.kata.user.web.controller.*.*(..))", returning = "returnValue")
     public void afterReturning(JoinPoint joinPoint, Object returnValue) throws JsonProcessingException {
 
         log.info("Inputs: ");
-        log.info(!ObjectUtils.isEmpty(joinPoint.getArgs()) ? JsonUtils.toJsonString(joinPoint.getArgs()[0]) : null);
+        String input = "";
+        if (!ObjectUtils.isEmpty(joinPoint.getArgs())) {
+            input = JsonUtils.toJsonString(joinPoint.getArgs()[0]);
+        }
+        log.info(input);
         log.info("Outputs: ");
-        log.info(JsonUtils.toJsonString(((ResponseEntity<?>) returnValue).getBody()));
-
+        String output = "";
+        if (!ObjectUtils.isEmpty(returnValue)) {
+            output = JsonUtils.toJsonString(((ResponseEntity<?>) returnValue).getBody());
+        }
+        log.info(output);
     }
 
     /**
      * This method executed after a failed execution of each method in the classes under web/controller package.
      * It contains logging of the input and the error raised by the method.
+     *
      * @param joinPoint {@link JoinPoint}
-     * @param e {@link Exception}
+     * @param e         {@link Exception}
      * @throws JsonProcessingException {@link JsonProcessingException}
      */
     @AfterThrowing(value = "execution(* com.kata.user.web.controller.*.*(..))", throwing = "e")
     public void afterThrowingAccessDeniedException(JoinPoint joinPoint, Exception e) throws JsonProcessingException {
         log.info("Inputs: ");
-        log.info(!ObjectUtils.isEmpty(joinPoint.getArgs()) ? JsonUtils.toJsonString(joinPoint.getArgs()[0]) : null);
+        String input = "";
+        if (!ObjectUtils.isEmpty(joinPoint.getArgs())) {
+            input = JsonUtils.toJsonString(joinPoint.getArgs()[0]);
+        }
+        log.info(input);
+
         log.info("Outputs: ");
         log.info(e.getMessage());
     }
 
     /**
      * This method executed after a successful/failed execution of each method in the classes under web/controller package.
-     * It contains logging of the method signature and its execution time.
-     * @param joinPoint {@link JoinPoint}
+     * It indicates the end of logging.
      */
     @After("execution(* com.kata.user.web.controller.*.*(..))")
-    public void afterCompletion(JoinPoint joinPoint) {
-        long executionTime = System.currentTimeMillis() - start;
+    public void afterCompletion() {
 
-        log.info("Method: " + joinPoint.getSignature() + " executed in " + executionTime + "ms");
         log.info("==============End Logging==============");
     }
 
