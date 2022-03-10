fbfe7ae5ceb3f5b2eaff5fed8f87848bb87e8c02
Hassib Maslah <maslah.has@gmail.com>
2022-03-10 20:35:26 +0100

    [UPDATE] add javadoc to custom exception

--- src/main/java/com/kata/user/web/exception/UserNotFoundException.java
+++ src/main/java/com/kata/user/web/exception/UserNotFoundException.java
@@ -1,5 +1,8 @@
 package com.kata.user.web.exception;
 
+/**
+ * This is custom {@link RuntimeException} that should be thrown when ever a user not found in the database.
+ */
 public class UserNotFoundException extends RuntimeException {
 
     public UserNotFoundException(String message) {
